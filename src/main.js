40829b7ddcae9ff409e303df1bc7b87dbabd9361
Vinh <vinh575@gmail.com>
2019-05-27 21:24:13 -0400

    Initial Firebase set up, hasn't been tested, probably doesn't work.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
+import * as firebase from 'firebase'
  
 Vue.use(VueGoogleMaps, {
   load: {
@@ -13,4 +14,16 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+
+  created () {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDX3JhPxVsNv9Vz3Jyi2R6Lil6E6KHJMV0",
+      authDomain: "hopperplan.firebaseapp.com",
+      databaseURL: "https://hopperplan.firebaseio.com",
+      projectId: "hopperplan",
+      storageBucket: "hopperplan.appspot.com",
+      messagingSenderId: "434194510357",
+      appId: "1:434194510357:web:e1b174ddd4bd861d"
+    });
+  }
 }).$mount('#app')
